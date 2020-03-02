efb2a24e6707bf5c464448745bf6515501e23b30
James Brind <jb753@cam.ac.uk>
2020-03-02 16:45:55 +0000

    First implementation struggling with NaN in Cholesky Re stress

--- sem.py
+++ sem.py
@@ -25,6 +25,14 @@ class SEM:
         self.uv = uv
         self.U = U
 
+        # Blend to the free-stream length scale
+        # Assume y is normalised on BL thickness
+        if Linf is None:
+            L = np.interp(y, [0., 1., 2.], [0., 0.41, 0.41])
+        else:
+            L = np.interp(y, [0., 1., 2 * Linf], [0., 0.41, Linf])
+        self.L = L[:, None, None, None]
+
         # Assemble Reynolds stress tensor
         self.R = np.zeros((np.size(y), 3, 3))
         self.R[:, 0, 0] = self.uu
@@ -32,14 +40,7 @@ class SEM:
         self.R[:, 2, 2] = self.ww
         self.R[:, 1, 0] = self.uv
         self.R[:, 0, 1] = self.uv
-        self.a = np.linalg.cholesky(self.R)
-
-        # Blend to the free-stream length scale
-        # Assume y is normalised on BL thickness
-        if Linf is None:
-            self.L = np.interp(y, [0., 1., 2.], [0., 0.41, 0.41])
-        else:
-            self.L = np.interp(y, [0., 1., 2 * Linf], [0., 0.41, Linf])
+        self.a = np.linalg.cholesky(self.R)[:, None, None, ...]
 
         # Set bounding box
         # Square centered around z=0, from 0 to ymax, at x=0, with Lmax space around it
@@ -58,9 +59,9 @@ class SEM:
         self.Nk = np.int(Dens * self.Vol / Lmax ** 3.)
 
         # Initialise eddies uniformly over box
-        self.xk = np.empty((self.Nk, 3))
+        self.xk = np.empty((1, 1, self.Nk, 3))
         for i in range(3):
-            self.xk[:, i] = np.random.uniform(bb[i, 0], bb[i, 1], (self.Nk,))
+            self.xk[..., i] = np.random.uniform(bb[i, 0], bb[i, 1], (self.Nk,))
 
         # Choose eddy orientations
         self.ek = np.random.choice((-1, 1), self.xk.shape)
@@ -70,19 +71,24 @@ class SEM:
 
         # Assemble input grid vector
         sg = np.shape(yg)
-        xg = np.stack((np.zeros_like(yg), yg, zg), 2)
+        xg = np.stack((np.zeros_like(yg), yg, zg), 2)[:, :, None, :]
 
         # Get distances to all eddies
-        dxk = (np.expand_dims(xg, 2) - np.expand_dims(np.expand_dims(self.xk, 0), 0))
-        dxk_l = np.swapaxes(np.swapaxes(dxk, 0, -1) / self.L, 0, -1)
+        dxk = (xg - self.xk) / self.L
 
         # Evaluate shape function
-        f = np.prod(np.where(dxk_l < 1., 0., 1. - dxk_l ** 2.), 3) / (self.L ** 3.) * np.sqrt(self.Vol)
+        f = np.prod(np.where(dxk < 1., 0., 1. - dxk ** 2.), -1, keepdims=True) / (self.L ** 3.) * np.sqrt(self.Vol)
 
         # Compute sum over all components of aij ekj
-        print(np.shape(self.a))
-        print(np.shape(self.ek))
-        aiek = self.a @ self.ek
+        u = np.einsum('...kij,...kj,...kl->...i', self.a, self.ek, f) / np.sqrt(self.Nk)
+
+        lev = np.linspace(-1.0, 1.0, 11)
+        f, a = plt.subplots()
+        a.contourf(yg, zg, u[..., 0], levels=lev, cmap='RdBu')
+        a.axis('equal')
+        plt.show()
+
+        return u
 
     def plot_input(self):
 
@@ -92,7 +98,7 @@ class SEM:
         a[0].plot(self.ww, self.y_t, 'x', label="$\overline{w\'w\'}$")
         a[0].plot(self.uv, self.y_t, 'x', label="$\overline{u\'v\'}$")
 
-        a[1].plot(self.L, self.y_t, 'kx')
+        a[1].plot(self.L.flatten(), self.y_t, 'kx')
 
         a[2].plot(self.U, self.y_t, 'kx')
 
@@ -109,10 +115,19 @@ class SEM:
 if __name__ == '__main__':
     # Load data and interpolate onto a common grid
     Dat = np.genfromtxt('Ziefle2013_Re_stress.csv', delimiter=',', skip_header=2)
+    for n in range(np.size(Dat, 1)):
+        for m in range(np.size(Dat, 0)):
+            if np.isnan(Dat[m, n]):
+                Dat[m:, n] = Dat[m - 1, n]
+                break
 
-    y_in = np.arctanh(np.linspace(0.001, .95, 51))
+    eps = 0.1
+    Dat[Dat < eps] = eps
+
+    y_in = np.arctanh(np.linspace(0.005, .95, 51))
     y_in = y_in / np.max(y_in) * 2.0
     U_in = np.where(y_in < 1., y_in ** (1. / 7.), 1.0) * 100.
+
     uu_in = np.interp(y_in, Dat[:, 0], Dat[:, 1])
     vv_in = np.interp(y_in, Dat[:, 2], Dat[:, 3])
     ww_in = np.interp(y_in, Dat[:, 4], Dat[:, 5])
@@ -120,11 +135,11 @@ if __name__ == '__main__':
 
     theSEM = SEM(y_in, U_in, uu_in, vv_in, ww_in, uv_in, .75)
 
+    theSEM.plot_input()
+
     zgv_in = np.linspace(-1., 1., 11)
     ygv_in = y_in
 
     yg_in, zg_in = np.meshgrid(zgv_in, ygv_in)
 
-    theSEM.evaluate(yg_in, zg_in)
-
-    # theSEM.plot_input()
+    u_out = theSEM.evaluate(yg_in, zg_in)
