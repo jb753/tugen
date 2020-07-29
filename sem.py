9ea97f56d51009d83966f66e70ad1d3ffa0a9610
James Brind <jb753@cam.ac.uk>
2020-07-29 12:18:52 +0100

    Homogenous isotropic turbulence case

--- sem.py
+++ sem.py
@@ -5,8 +5,222 @@ import matplotlib.pyplot as plt
 import math
 import time
 import json
-import importlib.resources as pkg_resources
-import multiprocessing
+
+
+# Eddy shape functions
+gaussian_norm = 1. / np.sqrt( math.erf(np.sqrt(np.pi * 2.)) / np.sqrt(2.) )
+def gaussian_shape(dxsq):
+    return gaussian_norm * np.exp(-np.pi * dxsq)
+
+quadratic_norm = 1. / np.sqrt(16. / 15.)
+def quadratic_shape(dxsq):
+    return quadratic_norm * (1. - dxsq)
+
+triangular_norm = 1. / np.sqrt(2. / 3.)
+def triangular_shape(dxsq):
+    return triangular_norm * (1. - np.sqrt(dxsq))
+
+class AnisoSEM:
+    """Generate correlated velocity fluctuations using the synthetic eddies."""
+
+    def __init__(self, y, z, uu, vv, ww, uv, L, Dens=100., fsh='quadratic'):
+        # Store input data
+        self.y = y
+        self.L = L
+
+        # Assemble Reynolds stress tensor
+        self.R = np.zeros((np.size(y), 3, 3))
+        self.R[:, 0, 0] = uu
+        self.R[:, 1, 1] = vv
+        self.R[:, 2, 2] = ww
+        self.R[:, 1, 0] = uv
+        self.R[:, 0, 1] = uv
+
+        # Make interpolator for Reynolds stress
+        self.fR = scipy.interpolate.interp1d(y, self.R, axis=0)
+
+        # Set bounding box
+        # Square centered around z=0, from 0 to ymax, at x=0, with Lmax space around it
+        Lmax = np.amax(L)
+        bb = np.array([[-Lmax, Lmax],
+                       [y[0] - Lmax, y[-1] + Lmax],
+                       [z[0] - Lmax, z[-1] + Lmax]])
+        self.box = bb
+
+        # Uniform probability density in homogenous dirns
+        self.px = 1. / (bb[0, 1] - bb[0, 0])
+        self.pz = 1. / (bb[2, 1] - bb[2, 0])
+
+        # Extend length scale distribution outside of box
+        self.y_ext = np.concatenate(([bb[1,0]], y, [bb[1,1]]))
+        self.L_ext = np.concatenate(([L[0]], L, [L[-1]]))
+
+        # Determine the eddy probability density in y dirn
+        # Proportional to reciprocal of L 
+        py_raw = 1. / self.L_ext
+        self.py = py_raw / np.trapz(py_raw, self.y_ext)
+        self.cpy = np.insert(
+                scipy.integrate.cumtrapz(self.py, self.y_ext), 0, 0)
+
+
+        # Calculate number of eddies
+        self.Vol = np.prod(np.diff(self.box, 1, 1))
+        self.Nk = np.int(Dens * self.Vol /  np.amax(self.L) ** 3.)
+
+        print("Eddy box dimensions: ", np.ptp(bb, 1))
+        print("Number of eddies: ", self.Nk)
+
+        # Initialise eddies positions using probability distributions
+        self.xk = np.empty((self.Nk, 3))
+        for i in [0, 2]:
+            self.xk[:, i] = np.random.uniform(bb[i, 0], bb[i, 1], (self.Nk,))
+        self.xk[:, 1] = np.interp(np.random.rand(self.Nk), self.cpy, self.y_ext)
+
+        # Get length scales associated with each eddy
+        self.lk = np.interp(self.xk[:, 1], self.y, self.L)[..., None]
+
+        # Choose eddy orientations
+        self.ek = np.random.choice((-1, 1), self.xk.shape)
+
+        # Evaluate total scaling factor for each eddy
+        pyk = np.interp(self.xk[:, 1], self.y_ext, self.py) 
+        self.sfk = np.sqrt(1. / pyk[None, None, :, None]
+                / self.px / self.pz / self.lk ** 3.) * self.ek
+
+        # Choose shape function 
+        if fsh == 'gaussian':
+            self.shape = gaussian_shape
+        elif fsh == 'quadratic':
+            self.shape = quadratic_shape
+        elif fsh == 'triangular':
+            self.shape = triangular_shape
+
+    def set_grid(self, yg, zg):
+        """Prepare to evaluate eddies over a particular grid."""
+
+        # Assemble input grid vector
+        self.xg = np.stack((np.zeros_like(yg), yg, zg), 2)[:, :, None, :]
+
+        # Get Reynolds stresses at grid points of interest
+        self.ag = np.linalg.cholesky( self.fR(yg) )
+
+    def evaluate(self, nproc=None):
+        """Evaluate fluctuating velocity associated with current eddies."""
+
+        # Get distances to all eddies
+        dxksq = ((self.xg - self.xk[None, None, ...])
+                / self.lk[None, None, ...]) ** 2.
+
+        # Evaluate shape function
+        f = np.where(dxksq < 1., self.shape(dxksq), 0.)
+
+        # Normalise shape function
+        fsig_ek = np.sum(np.prod(f, -1, keepdims=True) * self.sfk, 2)
+
+        # Compute sum
+        u = np.einsum('...ij,...j', self.ag, fsig_ek) / np.sqrt(self.Nk)
+        u = np.squeeze(u)
+
+        return u
+
+    def convect(self, dt):
+        """Move the eddies."""
+
+        # Time step is normalised by dt_hat = dt * u_tau / delta
+        self.xk[:, 0] = self.xk[:, 0] + dt
+
+        # Check if any eddies have left the box
+        has_left = self.xk[:, 0] > self.box[0, 1]
+        Nk_new = np.sum(has_left)
+
+        # Get new positions for fresh eddies
+        xk_new = np.zeros((Nk_new, 3))
+        xk_new[:, 0] = self.box[0, 0]  # x at upstream side
+        # y with nonuniform dist
+        xk_new[:, 1] = np.interp(np.random.rand(Nk_new), self.cpy, self.y_ext)
+        # z with uniform dist
+        xk_new[:, 2] = np.random.uniform(
+                self.box[2, 0], self.box[2, 1], (Nk_new,))
+
+        # Get length scales and orientations for new eddies
+        lk_new = np.interp(xk_new[:, 1], self.y_ext, self.L_ext)[..., None]
+        ek_new = np.random.choice((-1, 1), xk_new.shape)
+        pyk_new = np.interp(xk_new[:, 1], self.y_ext, self.py)
+
+        # Insert into grid
+        self.xk[has_left, :] = xk_new
+        self.lk[has_left, :] = lk_new
+        self.ek[has_left, :] = ek_new
+
+        self.sfk[:, :, has_left, :] = np.sqrt(
+                1. / pyk_new[None, None, :, None]
+                / self.px / self.pz / lk_new ** 3.) * ek_new
+
+    def loop(self, dt, Nt):
+
+        start_time = time.perf_counter()
+
+        u = np.zeros(np.shape(self.xg)[:2] + (3, Nt))
+        print('Time step %d/%d' % (0, Nt), end="")
+        for i in range(Nt):
+            if not np.mod(i, 50):
+                print('\r', end="")
+                print('Time step %d/%d' % (i, Nt), end="")
+            u[..., i] = self.evaluate()
+            self.convect(dt)
+
+        print('\nElapsed time:', time.perf_counter() - start_time, "seconds")
+
+        self.u = u
+        return
+
+    def plot_input(self):
+
+        f, a = plt.subplots(1, 3, sharey=True)
+        a[0].plot(self.R[:,0,0], self.y, 'x', label="$\overline{u\'u\'}$")
+        a[0].plot(self.R[:,1,1], self.y, 'x', label="$\overline{v\'v\'}$")
+        a[0].plot(self.R[:,2,2], self.y, 'x', label="$\overline{w\'w\'}$")
+        a[0].plot(-self.R[:,0,1], self.y, 'x', label="$-\overline{u\'v\'}$")
+
+        a[1].plot(self.L.flatten(), self.y, 'kx')
+
+        a[0].set_ylabel("$y/\delta$")
+        a[0].set_xlabel("Reynolds Stress, $\overline{u_i\'u_j\'}$")
+        a[1].set_xlabel("Length Scale, $\ell/\delta$")
+
+        a[2].plot(self.py, self.y)
+        a[2].plot(self.cpy, self.y)
+        a[2].set_xlabel(r"Probability Density, $p(y)$")
+
+        a[0].legend()
+        plt.tight_layout()
+        plt.show()
+
+    def plot_output(self,yg):
+
+        # yg = self.xg[0,:,0]
+
+        f, a = plt.subplots(1, 2)
+        a[0].plot(self.R[:,0,0], self.y, 'x', label="$\overline{u\'u\'}$")
+        a[0].plot(self.R[:,1,1], self.y, 'x', label="$\overline{v\'v\'}$")
+        a[0].plot(self.R[:,2,2], self.y, 'x', label="$\overline{w\'w\'}$")
+        a[0].plot(-self.R[:,0,1], self.y, 'x', label="$-\overline{u\'v\'}$")
+
+        # Calculate stats
+        uu = np.mean(self.u[..., 0, :] ** 2., (-2, -1))
+        vv = np.mean(self.u[..., 1, :] ** 2., (-2, -1))
+        ww = np.mean(self.u[..., 2, :] ** 2., (-2, -1))
+        uv = np.mean(self.u[..., 0, :] * self.u[..., 1, :], (-2, -1))
+
+        a[0].set_prop_cycle(None)
+
+        a[0].plot(uu, yg, 'o')
+        a[0].plot(vv, yg, 'o')
+        a[0].plot(ww, yg, 'o')
+        a[0].plot(-uv, yg, 'o')
+
+        plt.tight_layout()
+        plt.show()
 
 
 class SEM:
@@ -107,7 +321,7 @@ class SEM:
         self.fsh = fsh
 
     def evaluate(self, yg, zg, Pl=None):
-        """Evaluate fluctuating velocity field associated with the current eddies."""
+        """Evaluate fluctuating velocity associated with current eddies."""
 
         # Assemble input grid vector
         xg = np.stack((np.zeros_like(yg), yg, zg), 2)[:, :, None, :]
---
+++ test.py
@@ -0,0 +1,27 @@
+import numpy as np
+import sem
+
+yv = np.linspace(0.,1.,5)
+zv = np.linspace(0.,1.,5)
+Tu = 0.1 * np.ones_like(yv)
+uv = np.zeros_like(yv)
+L = np.ones_like(yv)*0.1
+
+theSEM = sem.AnisoSEM(yv, zv, Tu, Tu, Tu, uv, L, 1.)
+
+# theSEM.plot_input()
+
+zg, yg = np.meshgrid(zv, yv)
+
+theSEM.set_grid(yg, zg)
+
+nstep_cycle = 5
+ncycle = 2000
+dt = L[0]/nstep_cycle
+tau = L[0]*ncycle
+nt = int(tau/dt)
+
+theSEM.loop(dt, nt)
+
+theSEM.plot_output(yv)
+
